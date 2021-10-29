42753e4c179d761e8767d46b2c17275a3330b9ea
paultje1900 <paulboerma@live.nl>
2021-10-29 15:36:01 +0200

    :spakles: added animations

--- js/questions.js
+++ js/questions.js
@@ -1,7 +1,50 @@
-const questions = ["Hoe kunnen we de druk op de zorg verminderen?", "Wat vind u van vaccineren?", "Moeten rijkere mensen in de samenleving meer belasting betalen?"];
-const questionHTML = document.getElementById("question");
-questionHTML.innerHTML = questions[randomIntFromInterval(1, 3) - 1];
+//variables
+const questionPool = ["Hoe kunnen we de druk op de zorg verminderen?", "Wat vind u van vaccineren?", "Moeten rijkere mensen in de samenleving meer belasting betalen?"];
+const starterText = "Gespreksstarter:";
+const questionText = questionPool[randomIntFromInterval(1, 3) - 1];
+
+let questionHTML = document.getElementById("question");
+let starterHTML = document.getElementById('starter');
+let goalContainerHTML = document.getElementById('goal-container');
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+//functions
+let i = 0;
+function writeStarterText() {
+    if (i < starterText.length) {
+        starterHTML.innerHTML += starterText.charAt(i);
+        i++;
+        setTimeout(writeStarterText, 50);
+    }	
+}
+
+let x = 0;
+function writeQuestionText() {
+    if (x < questionText.length) {
+        questionHTML.innerHTML += questionText.charAt(x);
+        x++;
+        setTimeout(writeQuestionText, 30);
+    }	
+}
+
+function fadeInGoal() {
+    goalContainerHTML.style.opacity = 1;
+}
 
 function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+//execution  
+async function execution() {
+    writeStarterText();
+    await delay(1500)
+    writeQuestionText();
+    await delay(2000)
+    fadeInGoal();
+}
+
+execution();
+
+
